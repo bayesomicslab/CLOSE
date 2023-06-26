471df9cd89ca791d214c2bc8a38c643ca628e684
NickGalindo <ngalindoc@unal.edu.co>
2023-06-26 04:43:55 -0500

    adding some extras for pretokenization

---
+++ otherUtils/tokenizeEfficient.py
@@ -0,0 +1,34 @@
+from ..embeddingsModels import *
+from ..preprocessing import *
+import os
+import pickle
+
+print("LOADING DATASET")
+df = loadCloseDataset("./files_with_abstract_titles_block_0.csv")
+print("FINISHED LOADING DATASET")
+
+print("LOADING MODEL AND TOKENIZER")
+model, tokenizer = loadBertModelAndTokenizer("bert-base-uncased", "bert-base-uncased")
+print("LOADING MODEL AND TOKENIZER")
+
+print("PREPROCESSING FOR TPKENIZATION")
+ids = []
+data_str = []
+for _, row in df.iterrows():
+  ids.append(row["id"])
+  data_str.append(row["text"])
+print("FINISHED PREPROCESSING FOR TOKENIZATION")
+
+print("STARTING TOKENIZATION")
+tokenized_text = tokenizer(data_str, truncation=True, padding="max_length",  return_tensors="pt")
+print("FINISHED TOKENIZATION")
+
+print("POSTPROCESSING TOKENIZATION")
+tokenized_with_ids = {id: tokenized_text for id, tokenized_text in zip(ids, tokenized_text)}
+print("FINISHED POSTPROCESSING TOKENIZATION")
+
+print("SAVING TOKENIZATION")
+with open(os.path.join(".", "tokenized_text.pkl"), "wb") as file:
+  pickle.dump(tokenized_with_ids, file)
+  file.close()
+print("FINISHED SAVING TOKENIZATION")
