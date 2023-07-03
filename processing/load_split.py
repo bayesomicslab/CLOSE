deeda4bd1bcef5ea1478ecfc6f9814ef173e0e33
NickGalindo <ngalindoc@unal.edu.co>
2023-07-03 11:38:12 -0500

    adding ram split

--- processing/load_split.py
+++ processing/load_split.py
@@ -3,16 +3,98 @@ from typing import Callable, Dict, List, Tuple
 from transformers import PreTrainedModel
 import torch
 import gc
+import psutil
+import os
+import subprocess
+import pickle
 
+__base_ram_usage = 0
+__batch_ram_usage = -1
 
-def extractEmbeddingsLoadSplit(data: Tuple[List, Dict], model: PreTrainedModel, run: Callable, split_size: int = 1000):
+def __unloadRam(id_embeddings: List, extracted_embeddings: List, batch_num: int, save_dir: str=".", ram_use_limit_percentage: int = 90):
     """
-    Split the load on tokenized data to be run on a function
+    Save the batch of embeddings passed in into the save_dir if ram usage is being breached
+
+    :param embeddings: the embeddings to be saved
+    :param batch_num: the batch number this embeddings was produced from 
+    :param save_dir: the directory to save to
+    """
+
+    global __base_ram_usage
+    global __batch_ram_usage
+
+    if __batch_ram_usage == -1:
+        __batch_ram_usage = psutil.virtual_memory()[2] - __base_ram_usage
+
+    if psutil.virtual_memory()[2] + __batch_ram_usage > ram_use_limit_percentage:
+        os.makedirs(save_dir, exist_ok=True)
+        with open(os.path.join(save_dir, f"ids_and_embeddings_{batch_num}.pkl"), "wb") as file:
+            pickle.dump((id_embeddings, extracted_embeddings), file)
+            file.close()
+
+        id_embeddings.clear()
+        extracted_embeddings.clear()
+
+        subprocess.Popen("zip -r save_dir.zip save_dir && rm -rf save_dir", shell=True)
+
+        return True
+
+    return False
+
+def __processLoad(id_embeddings: List, extracted_embeddings: List, batch_ids: List, batch_tokenized: Dict, batch_num: int, model: PreTrainedModel, run: Callable, device: str = "cpu"):
+    """
+    process the corresponding load on the model and extract embeddings
+
+
+    :param id_embeddings: the ids of the embedding to save to, passed in by reference to make faster
+    :param extracted_embeddings: the extracted embedding to save to, passed in by reference to make faster
+    :param batch_id: the ids of the batch
+    :param batch_tokenized: the tokenized text of the batch
+    :param mode: the model to run with
+    :param run: the function to be run 
+    :param device: the device to send the tensors to
+    """
+
+    print(f"MOVING BATCH {batch_num} TO GPU")
+    tokenized_batch_to_device = {
+        "input_ids": torch.stack(batch_tokenized["input_ids"]).to(device),
+        "token_type_ids": torch.stack(batch_tokenized["token_type_ids"]).to(device),
+        "attention_mask": torch.stack(batch_tokenized["attention_mask"]).to(device)
+    }
+    print(f"FINISHED MOVING BATCH {batch_num} TO GPU")
+    
+    print(f"RUNNING BATCH {batch_num} EXTRACT EMBEDDINGS")
+    
+    embeddings_batch = run(model, (batch_ids, tokenized_batch_to_device))
+    print(embeddings_batch)
+    print(f"FINISHED RUNNING BATCH {batch_num} EXTRACT EMBEDDINGS")
+    
+    print(f"MOVING BATCH {batch_num} TO CPU")
+    for i, emb in zip(embeddings_batch[0], embeddings_batch[1]):
+        id_embeddings.append(i)
+        extracted_embeddings.append(emb.to("cpu"))
+    print(f"FINISHED MOVING BATCH {batch_num} TO CPU")
+    
+def __memoryCleanup():
+    print(f"COLLECTING GARBAGE")
+    gc.collect()
+    torch.cuda.empty_cache()
+    print(f"FINISHED COLLECTING GARBAGE")
+
+
+
+
+def extractEmbeddingsLoadSplit(data: Tuple[List, Dict], model: PreTrainedModel, run: Callable, split_size: int = 1000, save_dir: str = "."):
+    """
+    Split the load on tokenized data to be run on a function save to disk if ram is being overloaded
 
     :param data: the data to be run with load splitting
     :param model: the model to be passed into runnable
-    :param run: the function to be called with model and dat for the extraction of embeddings 
+    :param run: the function to be called with model and dat for the extraction of embeddings :param split_size: the size of the splits to be used
+    :param save_dir: the directory to save to when ram is being overflowed
     """
+
+    global __base_ram_usage
     
     torch.cuda.empty_cache()
 
@@ -28,39 +110,23 @@ def extractEmbeddingsLoadSplit(data: Tuple[List, Dict], model: PreTrainedModel, 
         tokenized_batch = {"input_ids": [], "token_type_ids": [], "attention_mask": []}
         cnt = 0
         cur_batch = 0
+        ram_batch = 0
+
+        __base_ram_usage = psutil.virtual_memory()[2]
     
         for id, input_ids, token_type_ids, attention_mask in zip(data[0], data[1]["input_ids"], data[1]["token_type_ids"], data[1]["attention_mask"]):
             if cnt == split_size:
-                print(f"MOVING BATCH {cur_batch} TO GPU")
-                tokenized_batch_to_device = {
-                    "input_ids": torch.stack(tokenized_batch["input_ids"]).to(device),
-                    "token_type_ids": torch.stack(tokenized_batch["token_type_ids"]).to(device),
-                    "attention_mask": torch.stack(tokenized_batch["attention_mask"]).to(device)
-                }
-                print(f"FINISHED MOVING BATCH {cur_batch} TO GPU")
-    
-                print(f"RUNNING BATCH {cur_batch} EXTRACT EMBEDDINGS")
-    
-                embeddings_batch = run(model, (id_batch, tokenized_batch_to_device))
-                print(embeddings_batch)
-                print(f"FINISHED RUNNING BATCH {cur_batch} EXTRACT EMBEDDINGS")
-    
-                print(f"MOVING BATCH {cur_batch} TO CPU")
-                for i, emb in zip(embeddings_batch[0], embeddings_batch[1]):
-                    id_embeddings.append(i)
-                    extracted_embeddings.append(emb.to("cpu"))
-                print(f"FINISHED MOVING BATCH {cur_batch} TO CPU")
-    
+                __processLoad(id_embeddings, extracted_embeddings, id_batch, tokenized_batch, cur_batch, model, run, device)
+
                 id_batch = []
                 tokenized_batch = {"input_ids": [], "token_type_ids": [], "attention_mask": []}
-    
                 cnt = 0
                 cur_batch += 1
-    
-                print(f"COLLECTING GARBAGE")
-                gc.collect()
-                torch.cuda.empty_cache()
-                print(f"FINISHED COLLECTING GARBAGE")
+
+                if __unloadRam(id_embeddings, extracted_embeddings, cur_batch, save_dir=os.path.join(".", f"ram_batch_{ram_batch}")):
+                    ram_batch += 1
+
+                __memoryCleanup()
     
             id_batch.append(id)
             tokenized_batch["input_ids"].append(input_ids)
@@ -69,5 +135,4 @@ def extractEmbeddingsLoadSplit(data: Tuple[List, Dict], model: PreTrainedModel, 
             cnt += 1
 
 
-        
     return id_embeddings, torch.stack(extracted_embeddings).to("cpu")
